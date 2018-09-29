caadcd099fd9394520d2be67b372c81fe15c9507
toucanbran <brandon.gomez89@gmail.com>
2018-09-28 20:42:57 -0700

    added weatherbit api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,14 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+import { weatherbitApiKey } from './environment.apiKeys';
 
 export const environment = {
   production: false
 };
 
 export const weatherBit = {
-  apiKey: '[ADD YOUR API KEY HERE]',
+  apiKey: weatherbitApiKey,
   urlBase: 'https://api.weatherbit.io/v2.0/forecast/daily'
 };
 
